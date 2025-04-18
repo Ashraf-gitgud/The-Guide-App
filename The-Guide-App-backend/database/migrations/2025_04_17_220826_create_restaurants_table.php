908f9dd553e1a01a5447a249f0dc3d0814a6a2be
Mfdal <maadimfdal@gmail.com>
2025-04-18 14:38:15 +0100

    backend v1.0.0

---
+++ The-Guide-App-backend/database/migrations/2025_04_17_220826_create_restaurants_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('restaurants', function (Blueprint $table) {
+            $table->id("restaurant_id");
+            $table->string("name");
+            $table->string("email")->unique();
+            $table->string("phone_number");
+            $table->string("adress");
+            $table->string("restaurant_rating");
+            $table->string("rating");
+            $table->string("status")->default("pending");
+            $table->foreignId('user_id')->constrained()->onDelete('cascade')->onUpdate("cascade");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('restaurants');
+    }
+};
