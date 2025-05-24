fc058cac4283b68c8234966c9ace10248986c73a
Ashraf <khalilou9achraf@gmail.com>
2025-05-24 18:42:52 +0100

    logo added

--- frontend/src/Components/Nav/Navbar.jsx
+++ frontend/src/Components/Nav/Navbar.jsx
@@ -1,25 +1,30 @@
 import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import styles from './Nav.module.css';
 
 const Nav = () => {
   const [isOpen, setIsOpen] = useState(false);
-
   const toggleNav = () => setIsOpen(!isOpen);
 
   return (
     <nav className={styles.nav}>
       <div className={styles.navBrand}>
-        <Link to="/">The Guide</Link>
+        <NavLink to="/">
+          <img src="/images/logo/logo_light.svg" alt="The Guide" />
+        </NavLink>
       </div>
       <button className={styles.navToggle} onClick={toggleNav}>
         <span className={styles.navIcon}></span>
       </button>
       <ul className={`${styles.navMenu} ${isOpen ? styles.navMenuOpen : ''}`}>
-        <li><Link to="/map" className={styles.navLink}>Map</Link></li>
-        <li><Link to="/about" className={styles.navLink}>About</Link></li>
-        <li><Link to="/contact" className={styles.navLink}>Contact</Link></li>
-        <li><Link to="/login" className={styles.navLink}>Login/Sign Up</Link></li>
+        <li><NavLink to="/map" className={styles.navLink} activeClassName={styles.active}>Map</NavLink></li>
+        <li className={styles.navDivider}></li>
+        <li><NavLink to="/about" className={styles.navLink} activeClassName={styles.active}>About</NavLink></li>
+        <li className={styles.navDivider}></li>
+        <li><NavLink to="/contact" className={styles.navLink} activeClassName={styles.active}>Contact</NavLink></li>
+        <li className={styles.navSignUp}>
+          <NavLink to="/login" className={styles.navLink} activeClassName={styles.active}>Login / Sign Up</NavLink>
+        </li>
       </ul>
     </nav>
   );
